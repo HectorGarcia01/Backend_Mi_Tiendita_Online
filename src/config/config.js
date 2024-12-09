120685a816175094be7caf6c56cad73d73caccad
HectorGarcia01 <hg2779948@gmail.com>
2024-12-09 14:16:31 -0600

    feat: se agregó el archivo config.js para cargar y exportar variables de entorno

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+    env: process.env.NODE_ENV || 'dev',
+    port: process.env.PORT || 3000,
+    dbUser: process.env.USER_DB,
+    dbPassword: process.env.PASSWORD_DB,
+    dbHost: process.env.HOST_DB,
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.PORT_DB,
+    secretSession: process.env.SECRET_SESSION
+};
